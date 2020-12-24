5ce14925dce1683be84fca88a63d5ee44dceaba3
sasha <sasha@guesty.com>
2020-12-24 07:44:49 +0200

    remove comment

--- pipes/toLowerCase.js
+++ pipes/toLowerCase.js
@@ -2,7 +2,7 @@ const { Transform } = require("stream");
 
 const init = () => new Transform({
   transform(chunk, encoding, callback) {
-    this.push(chunk.toString().toLowerCase()); // to string redundant?
+    this.push(chunk.toString().toLowerCase());
     callback();
   }
 });
